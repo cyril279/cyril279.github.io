9f8f51c06f3f6d24a0a3cb3563639beaec6471f1
Cyril <cyril.wiggins@proton.me>
2025-09-03 14:38:40 -0600

    inkcutDistrobox.sh: Name & formatting tweaks

--- hardware/inkcutDistrobox00.sh
+++ hardware/inkcutDistrobox00.sh
@@ -2,14 +2,14 @@
 set -e #exit if processes fail
 # define variables
 CONTAINER_NAME=inkcutBox # Name of distrobox & of distrobox definition file
-INKCUTBOX_HOME=$HOME/$CONTAINER_NAME #Path where $HOME of distrobox will be located
-PIPX_HOME=$INKCUTBOX_HOME/.local/share
+CONTAINER_HOME=$HOME/$CONTAINER_NAME #Path where $HOME of distrobox will be located
+PIPX_HOME=$CONTAINER_HOME/.local/share
 PIPX_INKCUT=venvs/inkcut/lib/python*/site-packages/inkcut
 #
-DBX_DEFN_INI=$(cat << EOF
+MANIFEST_INI=$(cat << EOF
 [$CONTAINER_NAME]
 image=docker.io/library/alpine:3.22
-home=$INKCUTBOX_HOME
+home=$CONTAINER_HOME
 pull=true
 additional_packages="gcc cups-dev musl-dev linux-headers"
 additional_packages="python3-dev pipx py3-qt5"
@@ -17,55 +17,51 @@ exported_bins="/usr/bin/pipx"
 exported_bins_path="\$HOME/.local/bin"
 EOF
 )
-# Verify values of variables with user
-echo "Current configuration:"
+# Display configuration for user verification
 echo ""
-echo "Name of container: $CONTAINER_NAME"
-echo "Home directory of container: $INKCUTBOX_HOME"
+echo "       description | variable name  | value"
+echo "-------------------+----------------+----------------"
+echo " name of container | CONTAINER_NAME | $CONTAINER_NAME"
+echo "container HOME dir | CONTAINER_HOME | $CONTAINER_HOME"
+echo "container manifest |  MANIFEST_INI  | (see below)"
 echo ""
-echo "Distrobox definition entry:"
+echo "$MANIFEST_INI"
 echo ""
-echo "$DBX_DEFN_INI"
-echo ""
-echo "definitial file $CONTAINER_NAME.ini will be written to the following path:"
-echo "$INKCUTBOX_HOME/$CONTAINER_NAME.ini"
+echo "manifest file will be written to the following path:"
+echo "$CONTAINER_HOME/$CONTAINER_NAME.ini"
 echo ""
 echo "To adjust these values, edit the variable definitions"
-echo "'CONTAINER_NAME' & 'INKCUTBOX_HOME' at the beginning of the script"
+echo "'CONTAINER_NAME' &/or 'CONTAINER_HOME'"
+echo "at the beginning of the script"
 echo ""
-read -p "Are you ready to proceed with the installation as detailed above? (yes/no)" user_input
-#
+# Pause for user input to proceed or exit
 read -p "Are you ready to proceed with the installation? (yes/no): " input_user
 case $input_user in
     [Yy]|[Yy][Ee][Ss])
     ;;
     [Nn]|[Nn][Oo])
     echo ""
-    echo "Okay, then let's not"
+    echo "Okay, exiting script."
+    echo ""
     exit
     ;;
 esac
-
 # create directory to isolate our distrobox $HOME files
-mkdir -p $INKCUTBOX_HOME
+mkdir -p $CONTAINER_HOME
 # create distrobox.ini file
-echo "$DBX_DEFN_INI" > $INKCUTBOX_HOME/$CONTAINER_NAME.ini
+echo "$MANIFEST_INI" > $CONTAINER_HOME/$CONTAINER_NAME.ini
 # Assemble the container per the variables & declarative ini file
-distrobox-assemble create --file $INKCUTBOX_HOME/$CONTAINER_NAME.ini
+distrobox-assemble create --file $CONTAINER_HOME/$CONTAINER_NAME.ini
 # Install inkcut into the distrobox container using pipx
 pipx install inkcut --system-site-packages
-
 echo ""
-echo "inkcut can be launched using the following command:"
+echo "Take a moment to verify that the following command launches inkcut"
 echo ""
 echo "distrobox-enter --name $CONTAINER_NAME -- sh -c '\$HOME/.local/bin/inkcut'"
-echo "Time: $(date -Iminutes) :: $1" >> timestamp.log
-echo ""
-echo "Take a moment to verify that the above command launches inkcut"
 echo ""
 echo "The next step is to copy an icon into the local directory, and create a desktop file so that inkcut can be launched like any other graphical app on the system"
 echo ""
-read -p "Type 'yes' when ready to proceed, or 'no' if things aren't right: " input_user
+read -p "Type 'yes' when ready to proceed, or 'no' to exit the script: " input_user
 case $input_user in
     [Yy]|[Yy][Ee][Ss])
     ;;
@@ -76,12 +72,8 @@ case $input_user in
     exit
     ;;
 esac
-
 # copy source icon for system use
-distrobox-enter --name inkcutBox -- sh -c \ 
-'cp $PIPX_HOME/$PIPX_INKCUT/res/media/inkcut.svg \
-/home/$USER/.local/share/icons'
-
+distrobox-enter --name $CONTAINER_NAME -- cp $PIPX_HOME/$PIPX_INKCUT/res/media/inkcut.svg /home/$USER/.local/share/icons
 # Create 'inkcut.desktop' (configured as shown below)
 cat >$HOME/.local/share/applications/inkcut.desktop <<EOL
 [Desktop Entry]
@@ -89,17 +81,16 @@ Name=Inkcut
 GenericName=Terminal entering Inkcut
 Comment=Terminal entering Inkcut
 Categories=Distrobox;System;Utility
-Exec=/usr/bin/distrobox-enter inkcutBox -- sh -c '\$HOME/.local/bin/inkcut'
+Exec=/usr/bin/distrobox-enter $CONTAINER_NAME -- sh -c '\$HOME/.local/bin/inkcut'
 Icon=$HOME/.local/share/icons/inkcut.svg
 Keywords=distrobox;
 NoDisplay=false
 Terminal=false
 Type=Application
 EOL
-
 # Clear/reset all variables
-unset INKCUTBOX_HOME
+unset CONTAINER_HOME
+unset CONTAINER_NAME
 unset PIPX_HOME
 unset PIPX_INKCUT
-unset DBX_DEFN_INI
-unset CONTAINER_NAME
+unset MANIFEST_INI
